0b9c8da65db3c46aa9c6ded4702e2765c62bd5cb
Arooj448 <aroojali448@gmail.com>
2024-12-21 20:19:38 +0000

    initial commit

---
+++ src/Api/Data.js
@@ -0,0 +1,17 @@
+    export const getPosts = async ()=>{
+    
+
+    const response= await fetch('https://jsonplaceholder.typicode.com/posts', {method: "Get",
+
+    });
+
+    return await response.json();
+
+};
+
+
+export const getRandomUser = async() =>{
+    const response =await fetch('https://randomuser.me/api/', {method:"Get"});  //randomuser api
+
+    return await response.json();
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { getPosts, getRandomUser } from './Api/Data';
+import { useState ,useEffect } from 'react';
+import Postcard from './Api/Component/Postcard';
+import Usercard from './Api/Component/Usercard';
 
 function App() {
+const[data , setData]= useState(null); // before the value is null when screen renders
+const[userData , setUserData]= useState(null); //this state is for users
+
+useEffect(()=> {
+  getPosts().then((posts)=>setData(posts))   // after mounted then data shown on screen
+
+    },[]);
+
+    useEffect(()=> {
+      getRandomUser().then((user)=>setUserData(user.results[0]))   // after mounted then data shown on screen
+    
+        },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    {userData && <Usercard  data={userData}/>}
+      {
+        data ? data.map ((e)=> <Postcard title={e.title} body={e.body}/>) : <p>No data</p>    // postcard render from postcard component
+      } 
+      
     </div>
   );
 }
